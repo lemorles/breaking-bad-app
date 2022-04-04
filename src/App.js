7083574d5cb8889ba67460c6fd5243444363692f
Leandro <leomoralesarce@gmail.com>
2022-04-04 16:38:46 -0300

    config redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import "./App.css";
-import img from "./breaking-bad.svg";
 
 function App() {
-  return (
-    <div className="container">
-      <h1>REPASO M2</h1>
-      <img src={img} alt="breaking bad" style={{ width: "300px" }} />
-      <h2>Aplicación creada con React, Redux y React Router DOM</h2>
-      <h3>TAs:</h3>
-      <div className="flex">
-        <p>José Mella</p>
-        <p>Leandro Morales</p>
-      </div>
-    </div>
-  );
+  return <div className="container">App</div>;
 }
 
 export default App;
